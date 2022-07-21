437f52dc81f21bec8594d422e4d23b385730a5b3
bshaw928 <94549347+bshaw928@users.noreply.github.com>
2022-07-21 11:22:23 -0400

    Update app.js
    Added newMemeContainer

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const form = document.querySelector("form");
 //listen for submit, load image from url
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  const newMemeContainer = document.createElement("div");
   image = document.createElement("img");
   image.setAttribute("src", imageUrl.value);
   image.setAttribute("height", 400);
   image.setAttribute("width", 400);
-  memeContainer.appendChild(image);
+  newMemeContainer.appendChild(image);
 
   //then get text and place inside new divs, place the new divs inside div with image
   let topTextValue = topText.value;
@@ -20,11 +21,11 @@ form.addEventListener("submit", function (e) {
   topDiv.innerHTML = topTextValue;
   const bottomDiv = document.createElement("div");
   bottomDiv.innerHTML = bottomTextValue;
-  memeContainer.appendChild(topDiv);
-  memeContainer.appendChild(bottomDiv);
+  newMemeContainer.appendChild(topDiv);
+  newMemeContainer.appendChild(bottomDiv);
 
   // style text and place text over the meme using css positioning
-  memeContainer.style.position = "relative";
+  newMemeContainer.style.position = "relative";
   topDiv.style.position = "absolute";
   bottomDiv.style.position = "absolute";
   bottomDiv.style.bottom = "20px";
@@ -40,27 +41,18 @@ form.addEventListener("submit", function (e) {
 
   // clear form inputs
   form.reset();
-  // or
-  // imageUrl.value = "";
-  // topText.value = "";
-  // bottomText.value = "";
 
   // add removal button
   let removeBtn = document.createElement("button");
   removeBtn.innerText = "Delete Meme";
-  memeContainer.appendChild(removeBtn);
+  newMemeContainer.appendChild(removeBtn);
 
-  //make removal button work. **This is a bit buggy- after deleting the meme, you cannot always resubmit the form without refreshing the page first. Also, if there are multiple memes present and you delete the second one, the delete button will no longer work for the first meme.
+  memeContainer.appendChild(newMemeContainer);
+
+  //make removal button work
   removeBtn.addEventListener("click", function (e) {
     if (e.target.tagName === "BUTTON") {
-      // memeContainer.remove();
-      image.remove();
-      topDiv.remove();
-      bottomDiv.remove();
-      e.target.remove();
-      // e.target.parentElement.remove();
+      newMemeContainer.remove();
     }
   });
 });
-
-// Notes: When creating subsequent memes, the text from the first/prior meme disappears. I have not figured out how to fix this yet. Also, if you try to save the meme, the text will not be saved, only the image.
